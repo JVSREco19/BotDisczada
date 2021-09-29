4b5bc65fc42a0dcf74b31e2ccf52a09ce5d61237
JVSREco19 <joao.victor.silveira.r@unifei.edu.br>
2021-09-29 16:50:08 -0300

    a

--- index.js
+++ index.js
@@ -40,4 +40,4 @@ for (const file of player) {
     client.player.on(file.split(".")[0], event.bind(null, client));
 };
 
-client.login('ODg3ODI1MzUzOTQ1NTMwNDE4.YUJxyw.dung4ZOsaaz3Y6CTDoVl6wvKESA');
+client.login(process.env.TOKEN);
